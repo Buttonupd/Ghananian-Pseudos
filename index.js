7931a726f7ae80c1bcc849eb546cf069d7aa672a
Buttonupd <Dankariuki0101@gmail.com>
2020-02-16 12:41:31 +0300

    js

--- index.js
+++ index.js
@@ -3,5 +3,11 @@ function getName(params) {
     var month = document.getElementById('month').value;
     var dayOfTheMonth = document.getElementById('day').value;
     VarDate = newDate(`${Year} - ${month} - ${dayOfTheMonth}`);
+    var male = document.getElementById('male');
+    var female = document.getElementById('female');
+    var result = document.getElementById('outcome');
+    var daysOfTheWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    var males = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
+    var females = ['Akosua', 'Adwoa', 'Abena', 'Akua', 'Yaa', 'Afua', 'Ama' ];
     
 }
