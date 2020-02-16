1912ebdad07750741dfaad5fe055226baaccac7c
Buttonupd <Dankariuki0101@gmail.com>
2020-02-16 13:46:03 +0300

    js structure

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
-function getName(params) {
-    var Year = document.getElementById('year born').value;
-    var month = document.getElementById('month').value;
-    var dayOfTheMonth = document.getElementById('day').value;
-    var date = newDate(`${Year} - ${month} - ${dayOfTheMonth}`);
-    var dayBorn = date.getDay();
+function getName() {
+    var year = document.getElementById("YOB").value;
+    var month = document.getElementById("month").value;
+    var dayOfTheMonth = document.getElementById("day").value;
+    var date = new Date(`${year} - ${month} - ${dayOfTheMonth}`);
+    var birthDay = date.getDay();
     var male = document.getElementById('male');
     var female = document.getElementById('female');
-    var result = document.getElementById('outcome');
+    var outcome = document.getElementById('outcome');
     var dayOfTheWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     var males = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
     var females = ['Akosua', 'Adwoa', 'Abena', 'Akua', 'Yaa', 'Afua', 'Ama' ];
     if (year == "" || year.Length < 4 || year.Length > 4) {
         alert('Please enter a valid year')
-    } else if (month < 0 || month < 12) {
+    } else if (month < 0 || month > 12) {
         alert("place a valid month")
     } else if (dayOfTheMonth < 0 || dayOfTheMonth > 31) {
         alert('valid day')
-    } else if (male.Checked == false && female.Checked == false) {
+    } else if (male.checked == false && female.checked == false) {
         alert('Specify')
-    } else if (male.Checked == true) {
-        outcome.innerHTML = "hello" + male(birthday) + dayOfTheWeek
+    } else if (male.checked == true) {
+        outcome.innerHTML = "hello" + males[birthDay] + dayOfTheWeek[birthDay]
+    } else if (female.checked == true) {
+        outcome.innerHTML = females[birthDay] + dayOfTheWeek[birthDay]
+    } else {
+        alert("run")
     }
     
 }
